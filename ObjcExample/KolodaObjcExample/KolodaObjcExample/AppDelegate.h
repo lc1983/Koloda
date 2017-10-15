0f70c8c1e1557ce38d7408b73918a333a41a7a96
LONG CHENG <chenglon@gmail.com>
2017-10-15 09:50:26 -0700

    Add Objc Example.

---
+++ ObjcExample/KolodaObjcExample/KolodaObjcExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KolodaObjcExample
+//
+//  Created by LONG CHENG on 10/15/17.
+//  Copyright © 2017 LONG CHENG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
